838062e56758af7d8639c15ff7cb278f4d3026ba
SPring <legoqqman228@gmail.com>
2025-09-17 13:43:34 +0300

    add third script

---
+++ script3.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+cd lab0
+
+cp hariyama5 slakoth8/drilbur/alakazamhariyama
+cp wynaut9 hippowdon0/koffingwynaut
+ln -s slakoth8 Copy_2
+cp -r hippowdon0 shroomish2/nincada
+cp hariyama5 shroomish2/nincada
+cp ponyta5 shroomish2/emboar/snivy
+cat shroomish2/hitmonchan hippowdon0/gothitelle/shelgon shroomish2/emboar/grumpig >> wynaut9_14
+ln wynaut9 hippowdon0/pansagewynaut
+ln -s ponyta5 slakoth8/drilbur/piplupponyta
+ln -s ponyta5 hippowdon0/gothitelle/shelgonponyta
+cp -r slakoth8 hippowdon0/mothim/linoone
+ln hariyama5 shroomish2/nincada/crawdaunthariyama
+ln -s slakoth8 Copy_100
+cat shroomish2/nincada/omanyte shroomish2/nincada/oddish hippowdon0/gothitelle/grotle >> hariyama5_51
