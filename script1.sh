3dc5e99198a95506cf2b73d59e0d3aae01f1ac67
SPring <legoqqman228@gmail.com>
2025-09-17 12:56:05 +0300

    rewrite script1

--- script1.sh
+++ script1.sh
@@ -20,89 +20,89 @@ touch slakoth8/scraggy/reuniclus
 touch slakoth8/blitzle/{kabuto,crawdaunt}
 touch slakoth8/drilbur/{weavile,alakazam,piplup}
 
-echo "Тип диеты Omnivore" > hariyama5
+echo "Тип диеты Omnivore" >> hariyama5
 
-echo "Ходы Block Drain Punch Fire" > hippowdon0/timburr
+echo "Ходы Block Drain Punch Fire" >> hippowdon0/timburr
 echo "Punch Helping Hand Ice Punch Knock Off Low Kick Sleep Talk Snore" >> hippowdon0/timburr
 echo "Superpower Thunderpunch" >> hippowdon0/timburr
 
-echo "Живет Grassland" > hippowdon0/mothim/nidoranM
+echo "Живет Grassland" >> hippowdon0/mothim/nidoranM
 
-echo "Тип" > hippowdon0/mothim/dusclops
+echo "Тип" >> hippowdon0/mothim/dusclops
 echo "покемона GHOST NONE" >> hippowdon0/mothim/dusclops
 
-echo "weight=27.6 height=24.0 atk=8" > hippowdon0/nidorina/timburr
+echo "weight=27.6 height=24.0 atk=8" >> hippowdon0/nidorina/timburr
 echo "def=6" >> hippowdon0/nidorina/timburr
 
-echo "Ходы Air Cutter Body Slam Double-Edge Endeavor Knock Off" > hippowdon0/gothitelle/doduo
+echo "Ходы Air Cutter Body Slam Double-Edge Endeavor Knock Off" >> hippowdon0/gothitelle/doduo
 echo "Mud-Slap Roost Sky Attack Steel Wing Sleep Talk Snore Swift Uproar Tri" >> hippowdon0/gothitelle/doduo
 echo "Attack" >> hippowdon0/gothitelle/doduo
 
-echo "Способности Smokescreen Leer Water Gun Focus Energy" > hippowdon0/gothitelle/seadra
+echo "Способности Smokescreen Leer Water Gun Focus Energy" >> hippowdon0/gothitelle/seadra
 echo "Bubblebeam Agility Twister Brine Hydro Pump Dragon Dance Dragon" >> hippowdon0/gothitelle/seadra
 echo "Pulse" >> hippowdon0/gothitelle/seadra
 
-echo "satk=6 sdef=5 spd=5" > hippowdon0/gothitelle/shelgon
+echo "satk=6 sdef=5 spd=5" >> hippowdon0/gothitelle/shelgon
 
-echo "Возможности" > hippowdon0/gothitelle/grotle
+echo "Возможности" >> hippowdon0/gothitelle/grotle
 echo "Overland=5" >> hippowdon0/gothitelle/grotle
 echo "Jump=1 Power=2 Intelligence=3 Sinker=0 Sprouter=0" >> hippowdon0/gothitelle/grotle
 
-echo "Ходы Dark" > hippowdon0/koffing
+echo "Ходы Dark" >> hippowdon0/koffing
 echo "Pulse Pain Split Rollout Sleep Talk Snore Spite Uproar Shock" >> hippowdon0/koffing
 echo "Wave" >> hippowdon0/koffing
 
-echo "Возможности Overland=6 Surface=4 Jump=4 Power=2" > hippowdon0/pansage
+echo "Возможности Overland=6 Surface=4 Jump=4 Power=2" >> hippowdon0/pansage
 echo "Intelligence=4" >> hippowdon0/pansage
 
-echo "Способности Tackle Growl Tail Whip Ember" > ponyta5
+echo "Способности Tackle Growl Tail Whip Ember" >> ponyta5
 echo "Flame Wheel Stomp Flame Charge Fire Spin Take Down Inferno Agility" >> ponyta5
 echo "Fire Blast Bounce Flare Blitz" >> ponyta5
 
-echo "Развитые способности Inner" > shroomish2/hitmonchan
+echo "Развитые способности Inner" >> shroomish2/hitmonchan
 echo "Focus" >> shroomish2/hitmonchan
 
-echo "Тип покемона PSYCHIC NONE" > shroomish2/emboar/grumpig
+echo "Тип покемона PSYCHIC NONE" >> shroomish2/emboar/grumpig
 
-echo "Возможности" > shroomish2/tangrowth
+echo "Возможности" >> shroomish2/tangrowth
 echo "Overland=8 Surface 4 Jump=2 Power 4 Intelligence=4 Sprouter=0" >> shroomish2/tangrowth
 echo "Reach=0" >> shroomish2/tangrowth
 
-echo "satk=9 sdef=6 spd=7" > shroomish2/nincada/grovyle
+echo "satk=9 sdef=6 spd=7" >> shroomish2/nincada/grovyle
 
-echo "Ходы Body Slam Counter" > shroomish2/nincada/medicham
+echo "Ходы Body Slam Counter" >> shroomish2/nincada/medicham
 echo "Double-Edge Drain Punch Dynamicpunch Fire Punch Focus Punch Gravity" >> shroomish2/nincada/medicham
 echo "Helping Hand Ice Punch Low Kick Magic Coat Mega Kick Mega Punch" >> shroomish2/nincada/medicham
 echo "Metronome Mud-Slap Pain Split Recycle Role Play Secret Power Seismic" >> shroomish2/nincada/medicham
 echo "Toss Signal Beam Sleep Talk Snore Swift Thunderpunch Trick Vacuum" >> shroomish2/nincada/medicham
 echo "Wave Zen Headbutt" >> shroomish2/nincada/medicham
 
-echo "Способности Torrent Overcharge Volt" > shroomish2/nincada/chinchou
+echo "Способности Torrent Overcharge Volt" >> shroomish2/nincada/chinchou
 echo "Absorb Illuminate" >> shroomish2/nincada/chinchou
 
-echo "Тип диеты Phototroph" > shroomish2/nincada/oddish
+echo "Тип диеты Phototroph" >> shroomish2/nincada/oddish
 
-echo "Тип покемона" > shroomish2/nincada/omanyte
+echo "Тип покемона" >> shroomish2/nincada/omanyte
 echo "ROCK WATER" >> shroomish2/nincada/omanyte
 
-echo "satk=9 sdef=6 spd=6" > shroomish2/nincada/crawdaunt
+echo "satk=9 sdef=6 spd=6" >> shroomish2/nincada/crawdaunt
 
-echo "Тип диеты" > slakoth8/scraggy/reuniclus
+echo "Тип диеты" >> slakoth8/scraggy/reuniclus
 echo "Omnivore" >> slakoth8/scraggy/reuniclus
 
-echo "Способности Mountain Peak Torrent Battle Armor Swift" > slakoth8/blitzle/kabuto
+echo "Способности Mountain Peak Torrent Battle Armor Swift" >> slakoth8/blitzle/kabuto
 echo "Swim" >> slakoth8/blitzle/kabuto
 
-echo "satk=9 sdef=6 spd=6" > slakoth8/blitzle/crawdaunt
+echo "satk=9 sdef=6 spd=6" >> slakoth8/blitzle/crawdaunt
 
-echo "Тип покемона DARK" > slakoth8/drilbur/weavile
+echo "Тип покемона DARK" >> slakoth8/drilbur/weavile
 echo "ICE" >> slakoth8/drilbur/weavile
 
-echo "Способности Mind Mold Synchronize Inner" > slakoth8/drilbur/alakazam
+echo "Способности Mind Mold Synchronize Inner" >> slakoth8/drilbur/alakazam
 echo "Focus" >> slakoth8/drilbur/alakazam
 
-echo "Способности Pound Growl Bubble Water Sport Peck" > slakoth8/drilbur/piplup
+echo "Способности Pound Growl Bubble Water Sport Peck" >> slakoth8/drilbur/piplup
 echo "Bubblebeam Bide Fury Attack Brine Whirlpool Mist Drill Peck Hydro" >> slakoth8/drilbur/piplup
 echo "Pump" >> slakoth8/drilbur/piplup
 
-echo "Тип диеты Herbivore" > wynaut9
+echo "Тип диеты Herbivore" >> wynaut9
